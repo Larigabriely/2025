036bebc22ece5362040d0f18fe426a3dff804ced
Larigabriely <143524767+Larigabriely@users.noreply.github.com>
2025-03-22 15:23:04 -0300

    12

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ document.addEventListener('DOMContentLoaded', function(){
 
     });
 });
-
-diminuiFonteBotao.addEventListener('click', function(){
+   
+   let tamanhoAtualFonte = - 0.1
+   diminuiFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte -= 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
-});
+ }
+   
+);
 
 const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
 const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
