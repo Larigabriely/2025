165b118aa9722e0c26676621a9cc332349ed3820
Larigabriely <143524767+Larigabriely@users.noreply.github.com>
2025-03-22 15:41:10 -0300

    20

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+}
 
-    let tamanhoAtualFonte = 1;
+  {  let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
